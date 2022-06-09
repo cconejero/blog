8b2ed806ee5ee103d4fe8713b3f2257b1bbf7d23
Claudio Conejero <conejero.claudio@gmail.com>
2022-06-08 21:28:23 -0300

    Section 2-12

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+    <article>
+
+        <?php echo $post ?>
+        
+    </article>
+
+    <a href="/">Go Back</a>
+
+</body>
